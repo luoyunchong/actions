4e7ec3108dd72aaafa1b4a382e611b3cd73da524
igeekfan <igeekfan@foxmail.com>
2024-10-20 15:06:34 +0800

    update Hackernews -> HackerNews

---
+++ src/HackerNews/AppOption.cs
@@ -0,0 +1,19 @@
+﻿namespace HackerNews;
+
+public class AppOption
+{
+    public string AppPrivateKey { get; set; }
+    public int AppIntegrationId { get; set; }
+    public int InstallationId { get; set; }
+    public string Owner { get; set; }
+    public string Repo { get; set; }
+
+    public HacknewsType HacknewsType { get; set; }
+}
+
+public enum HacknewsType
+{
+    Daily = 10,
+    Weekly = 20,
+    Monthly = 30
+}
